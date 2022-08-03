64394de4c5545c2cc3025a546a699b72b0e3579e
Martynas Negreckis <negreckis.martynas@gmail.com>
2022-08-03 20:51:53 +0300

    Insert note through POST

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import mysql from 'mysql2';
-import mainPage from './lib/mainPage.js';
+import {renderMainPage, insertNewNote} from './lib/mainPage.js';
 
 const app = express();
 const port = 8081;
 app.use(express.static('public'));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }));
 
-app.get('/', mainPage);
-
+app.get('/', renderMainPage);
+app.post('/', insertNewNote);
 
 app.listen(port, () => console.log(`Starting server on port ${port}`));
