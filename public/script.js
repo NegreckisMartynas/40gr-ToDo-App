78d3208c064cb0ff061fb159c4e346575addc451
Martynas Negreckis <negreckis.martynas@gmail.com>
2022-08-04 20:08:58 +0300

    Edit functionality with editable content

--- public/script.js
+++ public/script.js
@@ -1,4 +1,59 @@
+const savedText = {}
+
 function deleteNote(id) {
     fetch(`/?id=${id}`, {method: 'DELETE'})
         .then(res => window.location = res.url);
+}
+
+function editNote(id, element) {
+    const container = element.parentElement
+                             .parentElement;
+
+    const noteText = container.querySelector('.note-text');
+    noteText.contentEditable = true;
+
+    savedText[id] = noteText.innerText;
+
+    container.remove(noteText);
+
+    container.querySelectorAll('.buttons>button')
+             .forEach(button => button.classList.add('hidden'));
+    container.querySelectorAll('.edit')
+             .forEach(button => button.classList.remove('hidden'));
+
+    console.log(id, noteText);
+}
+
+function saveEdit(id, element) {
+    const container = element.parentElement
+                             .parentElement;
+
+    const noteText = container.querySelector('.note-text');
+    noteText.contentEditable = false;
+
+    fetch('/', {method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' }, 
+                body: JSON.stringify({id, note: noteText.innerText})}
+    )
+
+    container.querySelectorAll('.buttons>button')
+             .forEach(button => button.classList.add('hidden'));
+    container.querySelectorAll('.standard')
+             .forEach(button => button.classList.remove('hidden'));
+}
+
+function undoEdit(id, element) {
+    const container = element.parentElement
+                             .parentElement;
+
+    const noteText = container.querySelector('.note-text');
+    noteText.contentEditable = false;
+
+    noteText.innerText = savedText[id];
+    delete savedText[id];
+
+    container.querySelectorAll('.buttons>button')
+             .forEach(button => button.classList.add('hidden'));
+    container.querySelectorAll('.standard')
+             .forEach(button => button.classList.remove('hidden'));
 }
