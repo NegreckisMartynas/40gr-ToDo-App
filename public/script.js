02ca70f3740dea9b8a0a86a53314a3124d8835b8
Martynas Negreckis <negreckis.martynas@gmail.com>
2022-08-04 20:08:58 +0300

    Undo button

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const savedText = {}
+
 function deleteNote(id) {
     fetch(`/?id=${id}`, {method: 'DELETE'})
         .then(res => window.location = res.url);
@@ -9,6 +11,8 @@ function editNote(id, element) {
 
     const noteText = container.querySelector('.note-text');
     noteText.contentEditable = true;
+
+    savedText[id] = noteText.innerText;
 
     setTimeout(() => {
         noteText.focus();
@@ -42,6 +46,22 @@ function saveEdit(id, element) {
                 headers: { 'Content-Type': 'application/json' }, 
                 body: JSON.stringify({id, note: noteText.innerText})}
     )
+
+    container.querySelectorAll('.buttons>button')
+             .forEach(button => button.classList.add('hidden'));
+    container.querySelectorAll('.standard')
+             .forEach(button => button.classList.remove('hidden'));
+}
+
+function undoEdit(id, element) {
+    const container = element.parentElement
+                             .parentElement;
+
+    const noteText = container.querySelector('.note-text');
+    noteText.contentEditable = false;
+
+    noteText.innerText = savedText[id];
+    delete savedText[id];
 
     container.querySelectorAll('.buttons>button')
              .forEach(button => button.classList.add('hidden'));
