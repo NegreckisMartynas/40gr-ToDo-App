4c880ff9df3270f64994138304467f431c8e66c5
Martynas Negreckis <negreckis.martynas@gmail.com>
2022-08-10 19:04:29 +0300

    Starting registration and login pages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import {getNotes, addNote, deleteNote, updateNote} from './src/routes.js';
+import { handleLogin, handleRegistration, loginPage, startRegistration,  } from './src/user-routes.js';
 
 const app = express();
 const port = 8081;
@@ -19,5 +20,10 @@ app.get('/', getNotes);
 app.post('/', addNote);
 app.delete('/', deleteNote);
 app.patch('/', updateNote);
+
+app.get('/register', startRegistration);
+app.post('/register', handleRegistration);
+app.get('/login', loginPage);
+app.post('/login', handleLogin);
 
 app.listen(port, () => console.log(`Starting server on port ${port}`));
---
+++ src/user-routes.js
@@ -0,0 +1,20 @@
+
+//send registration page
+export function startRegistration(req, res) {
+
+}
+
+//register user or redirect back on error
+export function handleRegistration(req, res) {
+
+}
+
+//send login page
+export function loginPage(req, res) {
+
+}
+
+//login user or send back on error
+export function handleLogin(req, res) {
+
+}
